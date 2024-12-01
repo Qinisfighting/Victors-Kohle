06a5ed87678b6a1c65d9a6f50479baf3ecb5d5a9
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-12-01 16:52:45 +0100

    set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import "./App.css";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import PocketMoney from "@/components/pocketMoney";
+import Savings from "@/components/savings";
+import SignIn from "@/components/signIn";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Tabs defaultValue="account" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="pocketMoney">Pocket Money</TabsTrigger>
+            <TabsTrigger value="savings">Savings</TabsTrigger>
+            <TabsTrigger value="signIn">Sign In</TabsTrigger>
+          </TabsList>
+          <TabsContent value="pocketMoney">
+            <PocketMoney />
+          </TabsContent>
+          <TabsContent value="savings">
+            <Savings />
+          </TabsContent>
+          <TabsContent value="signIn">
+            <SignIn />
+          </TabsContent>
+        </Tabs>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
