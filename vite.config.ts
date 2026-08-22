db505394e2f1c4f505d7945b2a6a642300d53270
Yanqin Qualmann <yq.qualmann@gmail.com>
2026-08-22 13:11:32 +0200

    change to installable PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,36 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Victors Kohle",
+        short_name: "Victors Kohle",
+        description: "Nix Asche auf Tasche, oder was!??",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
